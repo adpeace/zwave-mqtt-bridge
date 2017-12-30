894a0d4c4b70fcb1dd719a197e1ebafa47fa430d
Andrew Peace <andrew.peace@cantab.net>
2017-12-30 19:16:15 +0000

    Generate long_description for PyPI from README.md.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import subprocess
 from setuptools import setup
+
+# Try to create an rst long_description from README.md:
+try:
+    args = 'pandoc', '--to', 'rst', 'README.md'
+    long_description = subprocess.check_output(args)
+    long_description = long_description.decode()
+except Exception as error:
+    print("WARNING: Couldn't generate long_description - is pandoc installed?")
+    long_description = None
 
 setup(name="zwave_mqtt_bridge",
       version="0.0.1",
@@ -7,6 +17,7 @@ setup(name="zwave_mqtt_bridge",
       author="Andy Peace",
       author_email="andrew.peace@gmail.com",
       license="MIT",
+      long_description=long_description,
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
           'License :: OSI Approved :: MIT License',
